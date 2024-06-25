da7afea2c2e8c59df1b61a8a97de457d359b155d
HB78 <78313137+HB78@users.noreply.github.com>
2024-06-25 17:54:09 +0200

    je pense avoir réglé le probleme de tailwind, il venait du package clsx cad cn que l'on met dans les classes tailwind, celui ci ne marchait pas, la je vais voir si le fait d'avoir mis du ternaire a la place corrige le code

--- components/MessageList.jsx
+++ components/MessageList.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 
 const MessageList = ({ messages, isLoading }) => {
@@ -12,23 +11,26 @@ const MessageList = ({ messages, isLoading }) => {
   }
   if (!messages) return <></>;
   if (!messages || !Array.isArray(messages))
-    return <>ceci nest pas un tableau</>;
+    return <>ceci n&apos;est pas un tableau</>;
 
   return (
     <div className="flex flex-col gap-3 px-4">
       {messages.map((message) => {
         return (
-          <div key={message.id} className="text-orange-300">
+          <div
+            key={message.id}
+            className={`flex ${
+              message.role === "USER"
+                ? "justify-end pl-10"
+                : "justify-start pr-10"
+            }`}
+          >
             <div
-              className={cn(
-                "rounded-xl text-sm shadow-md ring-1 ring-gray-900/10 p-3 max-w-[80%]",
-                {
-                  "bg-blue-600 text-white": message.role === "USER",
-                },
-                {
-                  "bg-gray-800 text-white": message.role === "SYSTEM",
-                }
-              )}
+              className={`rounded-xl text-sm shadow-md ring-1 ring-gray-900/10 p-3 max-w-[80%] ${
+                message.role === "USER"
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-800 text-white"
+              }`}
             >
               <p>{message.content}</p>
               <div className="text-xs text-blue-500 mt-1">
