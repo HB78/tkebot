95329a0fb508530c5f4a0d72dba854486f0c6fbb
HB78 <78313137+HB78@users.noreply.github.com>
2024-06-25 18:44:13 +0200

    j'ai du utiliser le useffect dans messagelist pour guetter les changements de role et j'ai donc du mettre messagelist en use client

--- components/MessageList.jsx
+++ components/MessageList.jsx
@@ -1,6 +1,14 @@
+"use client";
 import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const MessageList = ({ messages, isLoading }) => {
+  const [role, setRole] = useState("USER");
+
+  useEffect(() => {
+    setRole(messages.role);
+  }, [messages.role]);
+
   const isUserPosition = "flex justify-end pl-10";
   const isUserColor =
     "rounded-xl text-sm shadow-md ring-1 ring-gray-900/10 p-3 max-w-[80%] bg-blue-600 text-white";
@@ -25,15 +33,13 @@ const MessageList = ({ messages, isLoading }) => {
           <div
             key={message.id}
             className={
-              message.role === "USER"
-                ? isUserPosition
-                : "flex justify-start pr-10"
+              role === "USER" ? isUserPosition : "flex justify-start pr-10"
             }
           >
-            <div className={message.role === "USER" ? isUserColor : isBotColor}>
+            <div className={role === "USER" ? isUserColor : isBotColor}>
               <p>{message.content}</p>
               <div className="text-xs text-blue-500 mt-1">
-                {message.role === "SYSTEM" ? "tkebot" : null}
+                {role === "SYSTEM" ? "tkebot" : null}
               </div>
             </div>
           </div>
