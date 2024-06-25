ee55de1a790cf5270ca53e4fe25324c3ef4a9bb9
HB78 <78313137+HB78@users.noreply.github.com>
2024-06-25 18:59:32 +0200

    j'ai enlever le useeffect de messagelist et je l'ai laissé en client component

--- components/MessageList.jsx
+++ components/MessageList.jsx
@@ -1,14 +1,7 @@
 "use client";
 import { Loader2 } from "lucide-react";
-import { useEffect, useState } from "react";
 
 const MessageList = ({ messages, isLoading }) => {
-  const [role, setRole] = useState("USER");
-
-  useEffect(() => {
-    setRole(messages.role);
-  }, [messages]);
-
   const isUserPosition = "flex justify-end pl-10";
   const isUserColor =
     "rounded-xl text-sm shadow-md ring-1 ring-gray-900/10 p-3 max-w-[80%] bg-blue-600 text-white";
@@ -33,13 +26,15 @@ const MessageList = ({ messages, isLoading }) => {
           <div
             key={message.id}
             className={
-              role === "USER" ? isUserPosition : "flex justify-start pr-10"
+              message.role === "USER"
+                ? isUserPosition
+                : "flex justify-start pr-10"
             }
           >
-            <div className={role === "USER" ? isUserColor : isBotColor}>
+            <div className={message.role === "USER" ? isUserColor : isBotColor}>
               <p>{message.content}</p>
               <div className="text-xs text-blue-500 mt-1">
-                {role === "SYSTEM" ? "tkebot" : null}
+                {message.role === "SYSTEM" ? "tkebot" : null}
               </div>
             </div>
           </div>
